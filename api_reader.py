3e9e4a107bdccc86b0c24e511da06a023f6880d1
Liz Wong <anotherlizwong@gmail.com>
2019-04-28 00:09:52 -0400

    first attempt at date range management

--- api_reader.py
+++ api_reader.py
@@ -1,4 +1,5 @@
 import requests
+import arrow
 
 keywords = ["@date", "@all"]
 
@@ -7,7 +8,7 @@ def read_api(url, date_time_range, measures):
     reads in an api from a url
     returns the values of the measures
     :param url: an API url with the necessary url parameters included
-    :param date_time_range: The date and time range that is to be cared about
+    :param date_time_range: The date and time range that is to be cared about [start_date, end_date]
     :param measures: the values we care about from the json object
     :return: a json object containing the url, measures and their values only
     """
@@ -21,6 +22,8 @@ def read_api(url, date_time_range, measures):
         nav = measure.split('.')
         data_piece = data
 
+        # TODO get uom for values that have it and attach it to the values[measure] object
+
         for step in range(0, len(nav)):
             if step == len(nav) - 1:
                 # found the value we're looking for
@@ -28,8 +31,8 @@ def read_api(url, date_time_range, measures):
 
             elif nav[step][:4] == "@date":
                 # find the value that has the date time overlap provided
-                # find_relevant(date_time_range, nav[step+1], data_piece) # TODO
-                values[measure] = data_piece[nav[step+1]] # temp, just return the bucket of data
+                values[measure] = find_relevant(date_time_range, nav[step][4:], nav[step+1], data_piece)
+                # values[measure] = data_piece[nav[step+1]] # temp, just return the bucket of data
                 break
 
             elif nav[step] in keywords:
@@ -63,7 +66,36 @@ def average(keyword, values):
     return average
 
 
-def find_relevant(date_time_range, keyword, values):
-    pass
+def find_relevant(date_time_range, date_keyword, val_keyword, values):
+    """
+    Returns a single value that encompasses the datetime range sent
+    If multiple options in values fulfill this requirement, average and send back average
+    :param date_time_range: [start_datetime, end_datetime]
+    :param date_keyword: e.g. "validTime"
+    :param val_keyword: e.g. "value"
+    :param values: array of objects, e.g. [{'validTime': '2019-04-27T16:00:00+00:00/PT1H', 'value': 8}, ...]
+    :return: a single value encompassing the date_time_range
+    """
+    result = ""
+
+    valid_indicies = []
+    for i in range(len(values)):
+        time_range = values[i][date_keyword].split("/")
+
+        start = arrow.get(range[0])
+        span = 0
+        if len(time_range) > 1:
+            span = int(time_range[1][2:-1]) # e.g. if it's PT1H, span = 1
+        end = start.shift(hours=span)
+
+        if start <= date_time_range[0] and end >= date_time_range[1]:
+            valid_indicies.append(i)
+
+    if len(valid_indicies) > 1:
+        result = average(val_keyword, values[valid_indicies])
+    elif len(valid_indicies) == 1:
+        result = values[valid_indicies[0]][val_keyword]
+
+    return result
 
 
